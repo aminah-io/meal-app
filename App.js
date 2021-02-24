942db3a04ab5bf2d031ea4ff107b739b38d08aa7
Aminah Burch <hello@aminah.io>
2021-02-24 14:44:15 +0300

    Added redux packages and created reducers

--- App.js
+++ App.js
@@ -3,10 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import { enableScreens } from 'react-native-screens';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 import MealsNavigator from './navigation/MealsNavigator';
 
+import mealsReducer from './store/reducers/meals';
+
 enableScreens();
+
+const rootReducer = combineReducers({
+  meals: mealsReducer
+});
+
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
  Font.loadAsync({
@@ -29,7 +39,9 @@ export default function App() {
   }
 
   return (
-    <MealsNavigator />
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
   );
 }
 
